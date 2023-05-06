6cd205147f0a276caa42bb55065da5fd2a32f307
vojhab <voj.hab@proton.me>
2023-05-06 13:37:57 +0200

    Add getEmbeddings function

--- src/crawler.js
+++ src/crawler.js
@@ -180,3 +180,18 @@ module.exports.getDataAboutWebsite = async function (url, headers) {
     throw new Error("Error getting data about website: " + error.message);
   }
 };
+
+module.exports.getEmbeddings = async function (data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result[0];
+};
