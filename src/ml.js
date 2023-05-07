8247873f96f1bbf89acc29d7b5136f877f4f1397
vojhab <voj.hab@proton.me>
2023-05-07 12:36:56 +0200

    Rename huggingface.js to ml.js

--- src/ml.js
+++ src/ml.js
@@ -17,19 +17,3 @@ module.exports.getEmbeddings = async function (data) {
   await new Promise((resolve) => setTimeout(resolve, 4000)); // wait for 4 seconds to avoid rate limit
   return result;
 };
-
-module.exports.createAIDescription = async function (data) {
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
-      },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = await response.json();
-  await new Promise((resolve) => setTimeout(resolve, 4000)); // wait for 4 seconds to avoid rate limit
-  return result[0].summary_text;
-};
