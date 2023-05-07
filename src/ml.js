3fa141dbc3e49f4568a82b1a55360c0a0c1ee6a4
vojhab <voj.hab@proton.me>
2023-05-07 12:42:57 +0200

    Rewrite getEmbeddings

--- src/ml.js
+++ src/ml.js
@@ -5,17 +5,9 @@ const use = require("@tensorflow-models/universal-sentence-encoder");
 dotenv.config();
 
 module.exports.getEmbeddings = async function (data) {
-  const response = await fetch(
-    "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
-      },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = await response.json();
-  await new Promise((resolve) => setTimeout(resolve, 4000)); // wait for 4 seconds to avoid rate limit
+  const model = await use.load();
+  const sentences = data;
+  const embeddings = await model.embed(sentences);
+  let result = embeddings.arraySync()[0];
   return result;
 };
